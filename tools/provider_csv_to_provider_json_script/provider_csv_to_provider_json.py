9971b59a3800293879bb120cc04af2cd9360b764
Richard Julian <richard@rjulian.net>
2024-01-15 11:55:03 -0800

    Fix outputting of splunk conf file to stdout, standardize on required args

--- tools/provider_csv_to_provider_json_script/provider_csv_to_provider_json.py
+++ tools/provider_csv_to_provider_json_script/provider_csv_to_provider_json.py
@@ -3,7 +3,6 @@
 # Licensed under the Universal Permissive License v 1.0
 # as shown at https://oss.oracle.com/licenses/upl.
 
-import sys
 import argparse
 import csv
 import json
@@ -91,34 +90,21 @@ def convert_onug_csv_to_dictionary(input_file):
 
 def write_json_output(all_providers, output_file):
     """Output file with a json dump"""
-    with open(output_file, "w", encoding="utf-8") as outfile:
+    with open(f"./{output_file}.json", "w", encoding="utf-8") as outfile:
         json.dump(all_providers, outfile)
 
 
-def dict_to_splunk_conf(all_providers):
+def dict_to_splunk_conf(all_providers, output_prefix):
     """Creates splunk configuration file and outputs file contents to screen."""
-    with open("./props.conf", "w", encoding="utf-8") as outfile:
+    with open(f"./{output_prefix}.conf", "w", encoding="utf-8") as outfile:
         for provider, provider_values in all_providers.items():
             for _, source_values in provider_values["source"].items():
-                # print(source + " : " + str(source_values))
-                print(f"[{provider}:{source_values['sourceName']}]")
                 outfile.write(f"[{provider}:{source_values['sourceName']}]\n")
                 for alert_mapping, alert_mapping_values in source_values["alerts"][
                     "__default__"
                 ]["alertMapping"].items():
                     alias_prefix = "FIELDALIAS-csnf-"
                     prefix = alias_prefix + alert_mapping.replace(".", "-")
-                    print(
-                        prefix
-                        + " = "
-                        + '"'
-                        + alert_mapping_values["path"]
-                        + '"'
-                        + " ASNEW "
-                        + '"csnf.'
-                        + alert_mapping
-                        + '"'
-                    )
                     outfile.write(
                         prefix
                         + " = "
@@ -136,21 +122,34 @@ def execute_conversion():
     """Orchestrates conversion logic"""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", type=argparse.FileType("r"), dest="input_csv", help="Input CSV File"
+        "-i",
+        "--input",
+        type=argparse.FileType("r"),
+        dest="input_csv",
+        help="Input CSV File",
+        required=True,
     )
     parser.add_argument(
-        "-o", type=argparse.FileType("w"), dest="output_json", help="JSON Output prefix"
+        "-o",
+        "--output",
+        dest="output",
+        help="Output prefix for both JSON and Splunk format.",
+        required=True,
     )
+
     result = parser.parse_args()
 
-    if len(sys.argv) != 5:
-        parser.print_help()
-    else:
-        print(f"Input file is: {result.input_csv.name}")
-        print(f"Output file is: {result.output_json.name}")
-        all_providers_dict = convert_onug_csv_to_dictionary(result.input_csv.name)
-        write_json_output(all_providers_dict, result.output_json.name)
-        dict_to_splunk_conf(all_providers_dict)
+    if result.output.endswith(".json") or result.outputendswith(".conf"):
+        print(f"Stripping .json or .conf from output prefix: {result.output}\n")
+        result.output = result.output[:-5]
+
+    print(f"Input file is: {result.input_csv.name}")
+
+    print(f"JSON output file is: {result.output}.json")
+    print(f"Splunk output file is: {result.output}.conf")
+    all_providers_dict = convert_onug_csv_to_dictionary(result.input_csv.name)
+    write_json_output(all_providers_dict, result.output)
+    dict_to_splunk_conf(all_providers_dict, result.output)
 
 
 execute_conversion()
